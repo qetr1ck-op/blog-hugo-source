ab49b4c625b788347f7890259d10c4771f2c4e9a
qetr1ck <orestprustayko@gmail.com>
2017-03-26 23:13:06 +0300

    Publishing to gh-pages (publish.sh)

---
+++ scripts/gh-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+
+cd $DIR/..
+
+# if [[ $(git status -s) ]]
+# then
+#     echo "The working directory is dirty. Please commit any pending changes."
+#     exit 1;
+# fi
+
+echo "Deleting old publication"
+cd public
+shopt -s extglob
+rm -rf !(/.git)
+cd ..
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
