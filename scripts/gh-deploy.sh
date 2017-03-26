09dc8bc2a5a07af1d4cc58f0ec1d5969a8fc2b69
qetr1ck <orestprustayko@gmail.com>
2017-03-26 23:24:11 +0300

    add sh gh-deploy

--- scripts/gh-deploy.sh
+++ scripts/gh-deploy.sh
@@ -4,11 +4,11 @@ DIR=$(dirname "$0")
 
 cd $DIR/..
 
-# if [[ $(git status -s) ]]
-# then
-#     echo "The working directory is dirty. Please commit any pending changes."
-#     exit 1;
-# fi
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
 
 echo "Deleting old publication"
 cd public
@@ -20,5 +20,7 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-git add .
-git commit -m "Publishing to gh-pages (publish.sh)"
+cd public
+git status
+git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+git push origin gh-pages
