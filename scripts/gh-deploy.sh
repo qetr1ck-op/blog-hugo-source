1d202d62745be33365bcbefda66974a938c31621
qetr1ck <orestprustayko@gmail.com>
2017-04-09 12:04:34 +0300

    add seach functionality

--- scripts/gh-deploy.sh
+++ scripts/gh-deploy.sh
@@ -6,20 +6,23 @@ cd $DIR/..
 
 if [[ $(git status -s) ]]
 then
-    echo "The working directory is dirty. Please commit any pending changes."
+    echo "💾 The working directory is dirty. Please commit any pending changes."
     exit 1;
 fi
 
-echo "Deleting old publication"
+echo "💾 Deleting old publication"
 cd public
 shopt -s extglob
-rm -rf !(/.git)
+rm -rf !(/.git) #except .git
 cd ..
 
-echo "Generating site"
+echo "💾 Generating lunr.json"
+yarn run build:search
+
+echo "💾 Generating site"
 hugo
 
-echo "Updating gh-pages branch"
+echo "💾 Updating gh-pages branch"
 cd public
 git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 git push origin gh-pages
