a29afc0ec722bb1923c4fc71ccf128a475fa2d31
qetr1ck <orestprustayko@gmail.com>
2017-04-10 22:04:04 +0300

    add lesson5

--- scripts/gh-deploy.sh
+++ scripts/gh-deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+function printNewLine {
+    printf "\n"
+}
+
 DIR=$(dirname "$0")
 
 cd $DIR/..
@@ -15,16 +19,20 @@ cd public
 shopt -s extglob
 rm -rf !(/.git) #except .git
 cd ..
+printNewLine
 
 echo "💾 Generating and commit lunr.json"
 yarn run build:search
 git add --all && git commit -m "Udpate search content in lunr.json (publish.sh)"
 git push origin master
+printNewLine
 
 echo "💾 Generating site"
 hugo
+printNewLine
 
 echo "💾 Updating gh-pages branch"
 cd public
 git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 git push origin gh-pages
+printNewLine
