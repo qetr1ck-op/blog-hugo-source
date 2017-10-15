d4ce31a3af884474393548e8c59d5ff6367ad044
oop <orestprustayko@gmail.com>
2017-10-15 10:40:13 +0300

    deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,10 +1,40 @@
-const { spawn, exec } = require('child_process')
+const { execSync } = require('child_process')
+const { removeSync, moveSync, emptyDirSync } = require('fs-extra')
+const chalk = require('chalk')
+const log = (str) => console.log(chalk.bgMagenta(str))
+const logNewLine = () => console.log('\n')
 
-exec('git status -s', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-})
+log('💾 Checking git status')
+const gitStatus = execSync('git status -s')
+
+if (gitStatus) {
+   log('The working directory is dirty. Please commit any pending changes:')
+   console.log(gitStatus.toString())
+   return;
+}
+
+try {
+  logNewLine()
+  log('💾 Clean old publication')
+  moveSync('./public/.git', './temp-public-git')
+  emptyDirSync('./public')
+  moveSync('./temp-public-git', './public/.git')
+
+  logNewLine()
+  log('💾 Generating and commit lunr.json')
+  execSync('yarn run build:search')
+  execSync('git add --all && git commit -m "Update search content in lunr.json"')
+  execSync('git push origin master')
+
+  logNewLine()
+  log('💾 Generating site')
+  execSync('hugo')
+
+  log('💾 Updating gh-pages branch')
+  logNewLine()
+  execSync('cd public') 
+  execSync('git add --all && git commit -m "Publishing to gh-pages"') 
+  execSync('git push origin master') 
+} catch (error) {
+  log(error)
+}
