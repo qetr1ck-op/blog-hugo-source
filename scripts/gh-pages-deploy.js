bb9406fb94ae050f5292d933d232abc34e87fae6
oop <orestprustayko@gmail.com>
2017-10-15 10:45:55 +0300

    upd

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -4,7 +4,7 @@ const chalk = require('chalk')
 const log = (str) => console.log(chalk.bgMagenta(str))
 const logNewLine = () => console.log('\n')
 
-/* log('💾 Checking git status')
+log('💾 Checking git status')
 const gitStatus = execSync('git status -s')
 
 if (gitStatus.length) {
@@ -13,33 +13,24 @@ if (gitStatus.length) {
    return;
 }
 
-try {
-  logNewLine()
-  log('💾 Clean old publication')
-  moveSync('./public/.git', './temp-public-git')
-  emptyDirSync('./public')
-  moveSync('./temp-public-git', './public/.git')
+logNewLine()
+log('💾 Clean old publication')
+moveSync('./public/.git', './temp-public-git')
+emptyDirSync('./public')
+moveSync('./temp-public-git', './public/.git')
 
-  logNewLine()
-  log('💾 Generating and commit lunr.json')
-  execSync('yarn run build:search')
-  execSync('git add --all && git commit -m "Update search content in lunr.json"')
-  execSync('git push origin master')
-
-  logNewLine()
-  log('💾 Generating site')
-  execSync('hugo')
-
-  log('💾 Updating gh-pages branch')
-  logNewLine()
-  execSync('cd public') 
-  execSync('git add --all && git commit -m "Publishing to gh-pages"') 
-  execSync('git push origin master') 
-} catch (error) {
-  log(error)
-} */
-
+logNewLine()
 log('💾 Generating and commit lunr.json')
 execSync('yarn run build:search')
 execSync('git add --all && git commit -m "Update search content in lunr.json"')
 execSync('git push origin master')
+
+logNewLine()
+log('💾 Generating site')
+execSync('hugo')
+
+log('💾 Updating gh-pages branch')
+logNewLine()
+execSync('cd public') 
+execSync('git add --all && git commit -m "Publishing to gh-pages"') 
+execSync('git push origin master') 
